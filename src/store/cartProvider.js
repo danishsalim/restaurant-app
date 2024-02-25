cf89511cecbae112c52675ab8d4cdb5b4c8b9a23
Danish <danishsalim321@gmail.com>
2024-02-25 20:51:20 +0530

    addItemtoCart is added

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -7,7 +7,16 @@ const CartProvider = (props) => {
   const [items,setItems] = useState([]);
 
     const addItemTOCart = (id)=>{
-        
+      for(let i=0;i<items.length;i++)
+      {
+        if(id==items[i]['id']){
+          setItems((prev)=>{
+            prev[i]['quantity']=prev[i]['quantity']+1
+            return [...prev]
+          }) 
+          break;
+        }
+      }
     }
     const removeItemFromCart = (id)=>{
       for(let i=0;i<items.length;i++)
